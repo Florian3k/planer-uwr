52a891f32d986d6b8724d5e6fed04c14ddfef03c
Florian3k <24961583+Florian3k@users.noreply.github.com>
2020-09-13 14:05:24 +0200

    add react strict mode

--- planer-uwr-webapp/imports/ui/App.tsx
+++ planer-uwr-webapp/imports/ui/App.tsx
@@ -3,10 +3,12 @@ import { BrowserRouter, Link } from 'react-router-dom';
 import { AppRouter } from './AppRouter';
 
 export const App = () => (
-  <BrowserRouter>
-    <Link to="/">Index</Link>
-    <Link to="/hello">Hello</Link>
-    <Link to="/info">Info</Link>
-    <AppRouter />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <Link to="/">Index</Link>
+      <Link to="/hello">Hello</Link>
+      <Link to="/info">Info</Link>
+      <AppRouter />
+    </BrowserRouter>
+  </React.StrictMode>
 );
