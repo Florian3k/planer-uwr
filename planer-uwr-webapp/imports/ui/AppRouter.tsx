2e054961659dbe57f1286bd4c4f0a5325f1e4024
Florian3k <24961583+Florian3k@users.noreply.github.com>
2020-09-26 23:46:10 +0200

    add basic routing for plan page

--- planer-uwr-webapp/imports/ui/AppRouter.tsx
+++ planer-uwr-webapp/imports/ui/AppRouter.tsx
@@ -2,20 +2,27 @@ import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { LandingPage } from './pages/LandingPage';
 import { UserLandingPage } from './pages/UserLandingPage';
+import { PlanPage } from './pages/PlanPage';
 import { useTracker } from 'meteor/react-meteor-data';
 import { Meteor } from 'meteor/meteor';
 
 export const AppRouter = () => {
   const user = useTracker(() => Meteor.user());
+  const loggingIn = useTracker(() => Meteor.loggingIn());
+
+  console.log(user, loggingIn);
 
   return (
     <Routes>
       {user ? (
-        <Route path="/" element={<UserLandingPage />} />
+        <>
+          <Route path="/" element={<UserLandingPage />} />
+          <Route path="/plan/:planId" element={<PlanPage />} />
+        </>
       ) : (
         <Route path="/" element={<LandingPage />} />
       )}
-      <Route path="/*" element={<Navigate to="/" />} />
+      {!loggingIn && <Route path="/*" element={<Navigate to="/" />} />}
     </Routes>
   );
 };
