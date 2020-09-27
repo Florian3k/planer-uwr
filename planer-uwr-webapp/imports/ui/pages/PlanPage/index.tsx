435d319d0a9874a2c89674dbf17f2fc1ac0dbc8f
Florian3k <24961583+Florian3k@users.noreply.github.com>
2020-09-27 17:20:50 +0200

    add basic plan listing

--- planer-uwr-webapp/imports/ui/pages/PlanPage/index.tsx
+++ planer-uwr-webapp/imports/ui/pages/PlanPage/index.tsx
@@ -1,5 +1,30 @@
+import { Meteor } from 'meteor/meteor';
+import { useTracker } from 'meteor/react-meteor-data';
 import React from 'react';
+import { useParams } from 'react-router-dom';
+
+import { Plans } from '/imports/api/plans';
 
 export const PlanPage = () => {
-  return <div>Plan page</div>;
+  const { planId } = useParams();
+
+  const [plan, planReady] = useTracker(() => {
+    const sub = Meteor.subscribe('plans');
+    return [Plans.findOne(planId), sub.ready()];
+  });
+
+  if (!planReady) {
+    return <div>Wczytywanie planu...</div>;
+  }
+
+  if (!plan) {
+    return <div>Nie znaleziono planu :(</div>;
+  }
+
+  return (
+    <div>
+      <div>Nazwa: {plan.name}</div>
+      <div>Liczba semestrów: {plan.semesters.length}</div>
+    </div>
+  );
 };
--- planer-uwr-webapp/imports/ui/pages/UserLandingPage/UserPlans.tsx
+++ planer-uwr-webapp/imports/ui/pages/UserLandingPage/UserPlans.tsx
@@ -1,10 +1,26 @@
-import { Card, Elevation } from '@blueprintjs/core';
+import { Button, Card, Elevation } from '@blueprintjs/core';
+import { Meteor } from 'meteor/meteor';
+import { useTracker } from 'meteor/react-meteor-data';
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Plans } from '../../../api/plans';
 
 export const UserPlans = () => {
+  const plans = useTracker(() => {
+    Meteor.subscribe('plans');
+    return Plans.find().fetch();
+  });
+
   return (
     <Card elevation={Elevation.TWO}>
       <h2 style={{ textAlign: 'center', marginTop: 0 }}>Twoje plany</h2>
+      {plans.map(plan => (
+        <Link to={'/plan/' + plan._id} key={plan._id}>
+          <Card>
+            <h3>{plan.name}</h3>
+          </Card>
+        </Link>
+      ))}
     </Card>
   );
 };
