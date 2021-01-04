8e54c50373c83d51f3155ebbf18d597608212504
Florian3k <24961583+Florian3k@users.noreply.github.com>
2021-01-04 20:23:04 +0100

    WIP dnd

--- planer-uwr-webapp/imports/ui/pages/PlanPage/index.tsx
+++ planer-uwr-webapp/imports/ui/pages/PlanPage/index.tsx
@@ -2,6 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import { useTracker } from 'meteor/react-meteor-data';
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 
 import { Courses } from '/imports/api/courses';
 import { Plans } from '/imports/api/plans';
@@ -34,26 +35,59 @@ export const PlanPage = () => {
       <div>Nazwa: {plan.name}</div>
       <div>Liczba semestrów: {plan.semesters.length}</div>
       <br />
-      <div style={{ display: 'flex' }}>
-        <Listing courses={courses} />
-        {plan.semesters.map((semester) => (
-          <div style={{ minWidth: 200, maxWidth: 250, flexGrow: 1 }}>
-            {semester.isGap ? (
-              'wolne :)'
-            ) : (
-              <>
-                {semester.courses.length ? (
-                  semester.courses.map((course) => (
-                    <CourseWrapper course={course} />
-                  ))
-                ) : (
-                  <div>Brak przedmiotów</div>
-                )}
-              </>
-            )}
-          </div>
-        ))}
-      </div>
+      <DragDropContext
+        onDragEnd={(result, provided) => {
+          console.log(result, provided);
+        }}
+      >
+        <div style={{ display: 'flex' }}>
+          <Listing courses={courses} />
+          {plan.semesters.map((semester, semesterIndex) => (
+            <div
+              style={{ minWidth: 300, maxWidth: 350, flexGrow: 1 }}
+              key={semesterIndex.toString()}
+            >
+              {semester.isGap ? (
+                'wolne :)'
+              ) : (
+                <Droppable droppableId={semester.semesterNumber.toString()}>
+                  {(provided, stateSnapshot) => (
+                    <div
+                      style={{ minHeight: 600, border: '1px solid blue' }}
+                      ref={provided.innerRef}
+                      {...provided.droppableProps}
+                    >
+                      <pre>{JSON.stringify(stateSnapshot, null, 2)}</pre>
+                      {semester.courses.length ? (
+                        semester.courses.map((course, courseIndex) => (
+                          <Draggable
+                            draggableId={`${semester.semesterNumber}-${courseIndex}-${course.id}`}
+                            index={courseIndex}
+                            key={`${course.id}${semester.semesterNumber}`}
+                          >
+                            {(provided) => (
+                              <div
+                                ref={provided.innerRef}
+                                {...provided.draggableProps}
+                                {...provided.dragHandleProps}
+                              >
+                                <CourseWrapper course={course} />
+                              </div>
+                            )}
+                          </Draggable>
+                        ))
+                      ) : (
+                        <div>Brak przedmiotów</div>
+                      )}
+                      {provided.placeholder}
+                    </div>
+                  )}
+                </Droppable>
+              )}
+            </div>
+          ))}
+        </div>
+      </DragDropContext>
     </div>
   );
 };
